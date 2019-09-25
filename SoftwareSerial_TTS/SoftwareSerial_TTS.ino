bff9cb8917e8e50c904d65b109d0f080517e52ab
cnctechnet <acer5502@gmail.com>
2019-09-25 16:33:08 +0800

    添加上电初始化OK播报

--- SoftwareSerial_TTS/SoftwareSerial_TTS.ino
+++ SoftwareSerial_TTS/SoftwareSerial_TTS.ino
@@ -1,13 +1,13 @@
 /**
- * @file SoftwareSerial_TTS.ino 软串口和TTS通讯测试程序SG
+ * @file SoftwareSerial_TTS.ino 软串口和TTS语音模块通讯测试程序SG
  * @author igcxl (igcxl@qq.com)
  * @brief  软串口和TTS通讯测试程序
  * @note 硬串口接收到数据时软串口发送GBK编码“你好”，软串口接收到数据时发送到硬串口
  * Not all pins on the Mega and Mega 2560 support change interrupts,
  *so only the following can be used for RX:
- *10, 11, 12, 13, 50, 51, 52, 53, 62, 63, 64, 65, 66, 67, 68, 69
- * @version 0.5
- * @date 2019-09-24
+ *10, 11, 12, 13, 14, 15, 50, 51, 52, 53, A8 (62), A9 (63), A10 (64), A11 (65), A12 (66), A13 (67), A14 (68), A15 (69).
+ * @version 1.0
+ * @date 2019-09-25
  * @copyright Copyright © igcxl.com 2019
  * 
  */
@@ -24,10 +24,12 @@ void setup()
 //    ; // wait for serial port to connect. Needed for native USB port only
 //  }
   Serial.println("Goodnight moon!");
-  delay(100);
-
+  
   // set the data rate for the SoftwareSerial port
-  mySerial.begin(9600);
+   mySerial.begin(9600);
+    delay(50);//等待TTS上电启动
+//mySerial.write("<S>3");//最高语速3
+// delay(300);
      mySerial.write(0xB3); //初
     mySerial.write(0xF5);
     mySerial.write(0xCA); //始
